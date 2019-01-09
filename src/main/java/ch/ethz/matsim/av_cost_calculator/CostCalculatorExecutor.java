0879677ad833d7f97b9ca70b6fc9b5d29faac70c
Sebastian Hörl <hoerl.sebastian@gmail.com>
2019-01-09 22:33:13 +0100

    Make scenario configurable

--- src/main/java/ch/ethz/matsim/av_cost_calculator/CostCalculatorExecutor.java
+++ src/main/java/ch/ethz/matsim/av_cost_calculator/CostCalculatorExecutor.java
@@ -1,217 +1,238 @@
 package ch.ethz.matsim.av_cost_calculator;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.matsim.core.controler.OutputDirectoryHierarchy;
-
-import java.io.*;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CostCalculatorExecutor {
 	final private Logger logger = Logger.getLogger(CostCalculatorExecutor.class);
-	
-    final private URL sourceURL;
 
-    final private File workingDirectoryPath;
-    final private File originalRunScriptPath;
-    final private File runScriptPath;
-    final private File templatePath;
-    final private File inputPath;
-    final private File outputPath;
+	final private URL sourceURL;
 
-    final private Map<String, Integer> rowMapping = new HashMap<>();
+	final private File workingDirectoryPath;
+	final private File originalRunScriptPath;
+	final private File runScriptPath;
+	final private File templatePath;
+	final private File inputPath;
+	final private File outputPath;
 
-    public CostCalculatorExecutor(File workingDirectoryPath, URL sourceURL) {
-        this.sourceURL = sourceURL;
+	final private Map<String, Integer> rowMapping = new HashMap<>();
 
-        this.workingDirectoryPath = workingDirectoryPath;
+	public CostCalculatorExecutor(File workingDirectoryPath, URL sourceURL) {
+		this(workingDirectoryPath, sourceURL, null);
+	}
 
-        this.runScriptPath = new File(workingDirectoryPath, "Main.R");
-        this.originalRunScriptPath = new File(workingDirectoryPath, "Main_Original.R");
-        this.templatePath = new File(workingDirectoryPath, "Template.xlsx");
-        this.inputPath = new File(workingDirectoryPath, "Input.xlsx");
-        this.outputPath = new File(workingDirectoryPath, "results_main.csv");
+	public CostCalculatorExecutor(File workingDirectoryPath, URL sourceURL, URL overrideTemplateURL) {
+		this.sourceURL = sourceURL;
 
-        rowMapping.put("Area", 2);
-        rowMapping.put("VehicleType", 5);
-        rowMapping.put("FleetSize", 6);
-        rowMapping.put("electric", 7);
-        rowMapping.put("automated", 8);
-        rowMapping.put("fleetOperation", 9);
+		this.workingDirectoryPath = workingDirectoryPath;
 
-        rowMapping.put("ph_operationHours_av", 12);
-        rowMapping.put("ph_operationHours", 13);
-        rowMapping.put("ph_relActiveTime", 14);
-        rowMapping.put("ph_avOccupancy", 15);
-        rowMapping.put("ph_avSpeed", 16);
-        rowMapping.put("ph_avTripLengthPass", 17);
-        rowMapping.put("ph_relEmptyRides", 18);
-        rowMapping.put("ph_relMaintenanceRides", 19);
-        rowMapping.put("ph_relMaintenanceHours", 20);
-        
-        rowMapping.put("oph_operationHours_av", 23);
-        rowMapping.put("oph_operationHours", 24);
-        rowMapping.put("oph_relActiveTime", 25);
-        rowMapping.put("oph_avOccupancy", 26);
-        rowMapping.put("oph_avSpeed", 27);
-        rowMapping.put("oph_avTripLengthPass", 28);
-        rowMapping.put("oph_relEmptyRides", 29);
-        rowMapping.put("oph_relMaintenanceRides", 30);
-        rowMapping.put("oph_relMaintenanceHours", 31);
-        
-        rowMapping.put("ngt_operationHours_av", 34);
-        rowMapping.put("ngt_operationHours", 35);
-        rowMapping.put("ngt_relActiveTime", 36);
-        rowMapping.put("ngt_avOccupancy", 37);
-        rowMapping.put("ngt_avSpeed", 38);
-        rowMapping.put("ngt_avTripLengthPass", 39);
-        rowMapping.put("ngt_relEmptyRides", 40);
-        rowMapping.put("ngt_relMaintenanceRides", 41);
-        rowMapping.put("ngt_relMaintenanceHours", 42);
+		this.runScriptPath = new File(workingDirectoryPath, "Main.R");
+		this.originalRunScriptPath = new File(workingDirectoryPath, "Main_Original.R");
+		this.templatePath = new File(workingDirectoryPath, "Template.xlsx");
+		this.inputPath = new File(workingDirectoryPath, "Input.xlsx");
+		this.outputPath = new File(workingDirectoryPath, "results_main.csv");
 
-        setupWorkingDirectory();
-        updateWorkingDirectoryForR();
-    }
+		rowMapping.put("Scenario", 1);
+		rowMapping.put("Area", 2);
+		rowMapping.put("VehicleType", 5);
+		rowMapping.put("FleetSize", 6);
+		rowMapping.put("electric", 7);
+		rowMapping.put("automated", 8);
+		rowMapping.put("fleetOperation", 9);
 
-    private void setupWorkingDirectory() {
-    	logger.info("Setting up working directory at " + workingDirectoryPath);
-    	
-        try {
-            FileUtils.copyURLToFile(new URL(sourceURL, "CostCalculator.R"), new File(workingDirectoryPath, "CostCalculator.R"));
-            FileUtils.copyURLToFile(new URL(sourceURL, "Main.R"), new File(workingDirectoryPath, "Main_Original.R"));
-            FileUtils.copyURLToFile(new URL(sourceURL, "ScenarioAnalyzer.R"), new File(workingDirectoryPath, "ScenarioAnalyzer.R"));
-            FileUtils.copyURLToFile(new URL(sourceURL, "Input.xlsx"), new File(workingDirectoryPath, "Template.xlsx"));
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error setting up working directory for Cost Calculator");
-        }
-    }
-    
-    private static final String INITIAL_WORKING_DIRECTORY = "P:/_TEMP/Becker_Henrik/_AV-cost_comparison/Original_Suisse/CostCalculatorExtern/";
+		rowMapping.put("ph_operationHours_av", 12);
+		rowMapping.put("ph_operationHours", 13);
+		rowMapping.put("ph_relActiveTime", 14);
+		rowMapping.put("ph_avOccupancy", 15);
+		rowMapping.put("ph_avSpeed", 16);
+		rowMapping.put("ph_avTripLengthPass", 17);
+		rowMapping.put("ph_relEmptyRides", 18);
+		rowMapping.put("ph_relMaintenanceRides", 19);
+		rowMapping.put("ph_relMaintenanceHours", 20);
 
-    private void updateWorkingDirectoryForR() {
-    	logger.info("Updating working directory in R script");
-    	
-        try {
-            String script = FileUtils.readFileToString(originalRunScriptPath);
-            script = script.replace(INITIAL_WORKING_DIRECTORY, workingDirectoryPath.getAbsolutePath());
-            script = script.replace("Realizations_ZH", "Realizations");
-            FileUtils.writeStringToFile(runScriptPath, script);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		rowMapping.put("oph_operationHours_av", 23);
+		rowMapping.put("oph_operationHours", 24);
+		rowMapping.put("oph_relActiveTime", 25);
+		rowMapping.put("oph_avOccupancy", 26);
+		rowMapping.put("oph_avSpeed", 27);
+		rowMapping.put("oph_avTripLengthPass", 28);
+		rowMapping.put("oph_relEmptyRides", 29);
+		rowMapping.put("oph_relMaintenanceRides", 30);
+		rowMapping.put("oph_relMaintenanceHours", 31);
 
-    private void updateInputFile(Map<String, String> parameters) {
-    	logger.info("Updating input file...");
-        if (inputPath.exists()) inputPath.delete();
-        
-        parameters.put("oph_operationHours_av", "0");
-        parameters.put("oph_operationHours", "0");
-        parameters.put("oph_relActiveTime", "0");
-        parameters.put("oph_avOccupancy", "0");
-        parameters.put("oph_avSpeed", "0");
-        parameters.put("oph_avTripLengthPass", "0");
-        parameters.put("oph_relEmptyRides", "0");
-        parameters.put("oph_relMaintenanceRides", "0");
-        parameters.put("oph_relMaintenanceHours", "0");
-        
-        parameters.put("ngt_operationHours_av", "0");
-        parameters.put("ngt_operationHours", "0");
-        parameters.put("ngt_relActiveTime", "0");
-        parameters.put("ngt_avOccupancy", "0");
-        parameters.put("ngt_avSpeed", "0");
-        parameters.put("ngt_avTripLengthPass", "0");
-        parameters.put("ngt_relEmptyRides", "0");
-        parameters.put("ngt_relMaintenanceRides", "0");
-        parameters.put("ngt_relMaintenanceHours", "0");
+		rowMapping.put("ngt_operationHours_av", 34);
+		rowMapping.put("ngt_operationHours", 35);
+		rowMapping.put("ngt_relActiveTime", 36);
+		rowMapping.put("ngt_avOccupancy", 37);
+		rowMapping.put("ngt_avSpeed", 38);
+		rowMapping.put("ngt_avTripLengthPass", 39);
+		rowMapping.put("ngt_relEmptyRides", 40);
+		rowMapping.put("ngt_relMaintenanceRides", 41);
+		rowMapping.put("ngt_relMaintenanceHours", 42);
 
-        try {
-            XSSFWorkbook originalWorkbook = new XSSFWorkbook(templatePath);
-            originalWorkbook.write(new FileOutputStream(inputPath));
+		setupWorkingDirectory(overrideTemplateURL);
+		updateWorkingDirectoryForR();
+	}
 
-            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(inputPath));
-            XSSFSheet sheet = workbook.getSheet("Realizations");
+	private void setupWorkingDirectory(URL overrideTemplateURL) {
+		logger.info("Setting up working directory at " + workingDirectoryPath);
 
-            for (Map.Entry<String, Integer> mapping : rowMapping.entrySet()) {
-                if (!parameters.containsKey(mapping.getKey())) {
-                    throw new IllegalStateException("Parameter " + mapping.getKey() + " is missing");
-                }
+		try {
+			FileUtils.copyURLToFile(new URL(sourceURL, "CostCalculator.R"),
+					new File(workingDirectoryPath, "CostCalculator.R"));
+			FileUtils.copyURLToFile(new URL(sourceURL, "Main.R"), new File(workingDirectoryPath, "Main_Original.R"));
+			FileUtils.copyURLToFile(new URL(sourceURL, "ScenarioAnalyzer.R"),
+					new File(workingDirectoryPath, "ScenarioAnalyzer.R"));
 
-                sheet.getRow(mapping.getValue().intValue() - 1).getCell(1).setCellValue(parameters.get(mapping.getKey()));
-            }
+			if (overrideTemplateURL == null) {
+				FileUtils.copyURLToFile(new URL(sourceURL, "Input.xlsx"),
+						new File(workingDirectoryPath, "Template.xlsx"));
+			} else {
+				FileUtils.copyURLToFile(overrideTemplateURL, new File(workingDirectoryPath, "Template.xlsx"));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error setting up working directory for Cost Calculator");
+		}
+	}
 
-            workbook.write(new FileOutputStream(inputPath));
-        } catch (IOException | InvalidFormatException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error while updating Cost Calculator input");
-        }
-    }
+	private static final String INITIAL_WORKING_DIRECTORY = "P:/_TEMP/Becker_Henrik/_AV-cost_comparison/Original_Suisse/CostCalculatorExtern/";
 
-    private double readOutputFile(Map<String, String> parameters) {
-    	logger.info("Reading output file...");
-        Double result = null;
+	private void updateWorkingDirectoryForR() {
+		logger.info("Updating working directory in R script");
 
-        String item = parameters.get("electric").equals("1") ? "AE-S02" : "A-S02";
+		try {
+			String script = FileUtils.readFileToString(originalRunScriptPath);
+			script = script.replace(INITIAL_WORKING_DIRECTORY, workingDirectoryPath.getAbsolutePath());
+			script = script.replace("Realizations_ZH", "Realizations");
+			FileUtils.writeStringToFile(runScriptPath, script);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(outputPath)));
+	private void updateInputFile(Map<String, String> parameters) {
+		logger.info("Updating input file...");
+		if (inputPath.exists())
+			inputPath.delete();
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                if (line.contains(item)) {
-                	try {
-                		result = Double.parseDouble(line.split(";")[6]);
-                	} catch (NumberFormatException e) {
-                		result = Double.NaN;
-                		logger.warn("R script returned invalid expression (" + result + "), returning NaN");
-                	}
-                }
-            }
+		parameters.put("oph_operationHours_av", "0");
+		parameters.put("oph_operationHours", "0");
+		parameters.put("oph_relActiveTime", "0");
+		parameters.put("oph_avOccupancy", "0");
+		parameters.put("oph_avSpeed", "0");
+		parameters.put("oph_avTripLengthPass", "0");
+		parameters.put("oph_relEmptyRides", "0");
+		parameters.put("oph_relMaintenanceRides", "0");
+		parameters.put("oph_relMaintenanceHours", "0");
 
-            reader.close();
+		parameters.put("ngt_operationHours_av", "0");
+		parameters.put("ngt_operationHours", "0");
+		parameters.put("ngt_relActiveTime", "0");
+		parameters.put("ngt_avOccupancy", "0");
+		parameters.put("ngt_avSpeed", "0");
+		parameters.put("ngt_avTripLengthPass", "0");
+		parameters.put("ngt_relEmptyRides", "0");
+		parameters.put("ngt_relMaintenanceRides", "0");
+		parameters.put("ngt_relMaintenanceHours", "0");
 
-            if (result == null) {
-                throw new RuntimeException("Could not find result in Cost Calculator output");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new RuntimeException();
-        }
+		parameters.put("Scenario", "CCScenario");
 
-        return result;
-    }
+		try {
+			XSSFWorkbook originalWorkbook = new XSSFWorkbook(templatePath);
+			originalWorkbook.write(new FileOutputStream(inputPath));
 
-    private void runRScript() {
-    	logger.info("Running R script... ");
-        Runtime runtime = Runtime.getRuntime();
+			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(inputPath));
+			XSSFSheet sheet = workbook.getSheet("Realizations");
 
-        double startTime = System.nanoTime() * 1e-9;
-        
-        try {
-            Process process = runtime.exec("Rscript --vanilla Main.R", null, workingDirectoryPath);
+			for (Map.Entry<String, Integer> mapping : rowMapping.entrySet()) {
+				if (!parameters.containsKey(mapping.getKey())) {
+					throw new IllegalStateException("Parameter " + mapping.getKey() + " is missing");
+				}
 
-            while (process.isAlive()) {
-                Thread.sleep(10);
+				sheet.getRow(mapping.getValue().intValue() - 1).getCell(1)
+						.setCellValue(parameters.get(mapping.getKey()));
+			}
 
-                if (System.nanoTime() * 1e-9 - startTime > 30) {
-                	logger.info("Waiting for cost calculator ...");
-                }
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Error while executing Cost Calculator script");
-        }
-    }
+			workbook.write(new FileOutputStream(inputPath));
+		} catch (IOException | InvalidFormatException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error while updating Cost Calculator input");
+		}
+	}
 
-    public double computePricePerPassengerKm(Map<String, String> parameters) {
-        updateInputFile(parameters);
-        runRScript();
-        return readOutputFile(parameters);
-    }
+	private double readOutputFile(Map<String, String> parameters) {
+		logger.info("Reading output file...");
+		Double result = null;
+
+		String item = parameters.get("electric").equals("1") ? "AE-CCScenario" : "A-CCScenario";
+
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(outputPath)));
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				if (line.contains(item)) {
+					try {
+						result = Double.parseDouble(line.split(";")[6]);
+					} catch (NumberFormatException e) {
+						result = Double.NaN;
+						logger.warn("R script returned invalid expression (" + result + "), returning NaN");
+					}
+				}
+			}
+
+			reader.close();
+
+			if (result == null) {
+				throw new RuntimeException("Could not find result in Cost Calculator output");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+
+		return result;
+	}
+
+	private void runRScript() {
+		logger.info("Running R script... ");
+		Runtime runtime = Runtime.getRuntime();
+
+		double startTime = System.nanoTime() * 1e-9;
+
+		try {
+			Process process = runtime.exec("Rscript --vanilla Main.R", null, workingDirectoryPath);
+
+			while (process.isAlive()) {
+				Thread.sleep(10);
+
+				if (System.nanoTime() * 1e-9 - startTime > 30) {
+					logger.info("Waiting for cost calculator ...");
+				}
+			}
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error while executing Cost Calculator script");
+		}
+	}
+
+	public double computePricePerPassengerKm(Map<String, String> parameters) {
+		updateInputFile(parameters);
+		runRScript();
+		return readOutputFile(parameters);
+	}
 }
--- src/test/java/ch/ethz/matsim/av_cost_calculator/TestCostCalculatorExecutor.java
+++ src/test/java/ch/ethz/matsim/av_cost_calculator/TestCostCalculatorExecutor.java
@@ -1,46 +1,87 @@
 package ch.ethz.matsim.av_cost_calculator;
 
-import org.junit.Test;
-
-import org.junit.Assert;
-
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCostCalculatorExecutor {
-    @Test
-    public void testCostCalculatorExecutor() throws MalformedURLException, URISyntaxException {
-        URL sourceURL = getClass().getResource("Main.R").toURI().resolve(".").toURL();
+	@Test
+	public void testCostCalculatorExecutor() throws URISyntaxException, IOException {
+		URL sourceURL = getClass().getResource("Main.R").toURI().resolve(".").toURL();
 
-        File workingDirectory = new File("output/test/cost_calculator_test");
-        workingDirectory.mkdirs();
+		File workingDirectory = new File("output/test/cost_calculator_test");
+		workingDirectory.mkdirs();
 
-        CostCalculatorExecutor executor = new CostCalculatorExecutor(workingDirectory, sourceURL);
+		CostCalculatorExecutor executor = new CostCalculatorExecutor(workingDirectory, sourceURL);
 
-        Map<String, String> parameters = new HashMap<>();
+		Map<String, String> parameters = new HashMap<>();
 
-        parameters.put("Area", "Urban");
-        parameters.put("VehicleType", "Midsize");
-        parameters.put("FleetSize", "1500");
-        parameters.put("electric", "1");
-        parameters.put("automated", "1");
-        parameters.put("fleetOperation", "1");
+		parameters.put("Area", "Urban");
+		parameters.put("VehicleType", "Midsize");
+		parameters.put("FleetSize", "1500");
+		parameters.put("electric", "1");
+		parameters.put("automated", "1");
+		parameters.put("fleetOperation", "1");
 
-        parameters.put("ph_operationHours_av", "3.8");
-        parameters.put("ph_operationHours", "0");
-        parameters.put("ph_relActiveTime", "57");
-        parameters.put("ph_avOccupancy", "65");
-        parameters.put("ph_avSpeed", "20.6");
-        parameters.put("ph_avTripLengthPass", "3.4");
-        parameters.put("ph_relEmptyRides", "8");
-        parameters.put("ph_relMaintenanceRides", "5");
-        parameters.put("ph_relMaintenanceHours", "5");
+		parameters.put("ph_operationHours_av", "3.8");
+		parameters.put("ph_operationHours", "0");
+		parameters.put("ph_relActiveTime", "57");
+		parameters.put("ph_avOccupancy", "65");
+		parameters.put("ph_avSpeed", "20.6");
+		parameters.put("ph_avTripLengthPass", "3.4");
+		parameters.put("ph_relEmptyRides", "8");
+		parameters.put("ph_relMaintenanceRides", "5");
+		parameters.put("ph_relMaintenanceHours", "5");
 
-        double result = executor.computePricePerPassengerKm(parameters);
-        Assert.assertEquals(0.5562841614, result, 1e-6);
-    }
+		double result = executor.computePricePerPassengerKm(parameters);
+		Assert.assertEquals(0.5562841614, result, 1e-6);
+
+		FileUtils.deleteDirectory(workingDirectory);
+	}
+
+	@Test
+	public void testCostCalculatorExecutorWithCustomTemplate() throws URISyntaxException, IOException {
+		File workingDirectory = new File("output/test/cost_calculator_test");
+		workingDirectory.mkdirs();
+
+		// Prepare template
+		URL templateUrl = new URL(getClass().getResource("InputBerlin.xlsx").toURI().resolve(".").toURL(),
+				"InputBerlin.xlsx");
+		URL sourceURL = getClass().getResource("Main.R").toURI().resolve(".").toURL();
+
+		// Call executor
+
+		CostCalculatorExecutor executor = new CostCalculatorExecutor(workingDirectory, sourceURL, templateUrl);
+
+		Map<String, String> parameters = new HashMap<>();
+
+		parameters.put("Area", "Urban");
+		parameters.put("VehicleType", "Solo_Berlin");
+		parameters.put("FleetSize", "1500");
+		parameters.put("electric", "1");
+		parameters.put("automated", "1");
+		parameters.put("fleetOperation", "1");
+
+		parameters.put("ph_operationHours_av", "3.8");
+		parameters.put("ph_operationHours", "0");
+		parameters.put("ph_relActiveTime", "57");
+		parameters.put("ph_avOccupancy", "65");
+		parameters.put("ph_avSpeed", "20.6");
+		parameters.put("ph_avTripLengthPass", "3.4");
+		parameters.put("ph_relEmptyRides", "8");
+		parameters.put("ph_relMaintenanceRides", "5");
+		parameters.put("ph_relMaintenanceHours", "5");
+
+		double result = executor.computePricePerPassengerKm(parameters);
+		Assert.assertEquals(1.26256476770728, result, 1e-6);
+
+		FileUtils.deleteDirectory(workingDirectory);
+	}
 }
